df5196162cad9973dcab3bac1c017e3da4d71926
chkvdm <vadikvr6@outlook.com>
2025-11-24 13:00:07 +0300

    feat: add JSON model and bind data to view

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,16 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
+  (Controller, JSONModel) => {
+    'use strict';
 
-    return Controller.extend("epamsapui5task2.controller.Main", {
-        onInit() {
-        }
+    return Controller.extend('epamsapui5task2.controller.Main', {
+      onInit() {
+        const oBooksModel = new JSONModel(
+          sap.ui.require.toUrl('epamsapui5task2/model/books.json')
+        );
+
+        this.getView().setModel(oBooksModel, 'books');
+      },
     });
-});
+  }
+);
